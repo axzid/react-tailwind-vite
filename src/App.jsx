d192ffcf17b75785388bc6ecf162b6df73e10de8
zul id <storyciks@gmail.com>
2022-09-08 19:53:32 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-xl text-center">starter tailwind react js</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,jsx,ts,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
